dee940d17d3e5c72d81c8f53652e5059148f6072
Yaroslav <ya.kosteckiy@gmail.com>
2016-05-15 12:22:13 +0300

    add new admin, frontend classes

---
+++ application/libraries/Admin_Controller.php
@@ -0,0 +1,13 @@
+<?php
+
+class Admin_Controller extends MY_Controller
+{
+
+    /**
+     * Admin_Controller constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
---
+++ application/libraries/Frontend_Controller.php
@@ -0,0 +1,13 @@
+<?php
+
+class Frontend_Controller extends MY_Controller
+{
+
+    /**
+     * Frontend_Controller constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
