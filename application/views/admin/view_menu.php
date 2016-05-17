de3fb8c311485793538d898394dda41272c4dbc1
Yaroslav <ya.kosteckiy@gmail.com>
2016-05-18 00:34:30 +0300

    add member menu and admin dashboard

---
+++ application/views/admin/view_menu.php
@@ -0,0 +1,12 @@
+<li class="dropdown">
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Admin Menu <span class="caret"></span></a>
+    <ul class="dropdown-menu">
+        <li><?php echo anchor('admin/dashboard/users','Users Menu'); ?></li>
+        <li><?php echo anchor('admin/dashboard/posts','Posts Menu'); ?></li>
+        <li><?php echo anchor('admin/dashboard/pages','Pages Menu'); ?></li>
+        <li role="separator" class="divider"></li>
+        <li><a href="#">Separated link</a></li>
+        <li role="separator" class="divider"></li>
+        <li><a href="#">Separated link</a></li>
+    </ul>
+</li>
