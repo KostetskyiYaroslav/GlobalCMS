fd79dd0ea5a119734a8a3cc5c7db0f1aa0070a33
Yaroslav <ya.kosteckiy@gmail.com>
2016-05-18 00:38:03 +0300

    create personal controller(necessary data init) and model(add CRUID functionalyti)

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -10,7 +10,28 @@ class MY_Controller extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        $this->data['error'] = array();
-        $this->data['site_name'] = config_item('site_name');
+
+        $this->load->model('Model_users');
+        $this->load->model('Model_posts');
+
+        $this->data['site_name']    = config_item('site_name');
+        $this->data['error']        = array();
+        $this->data['auth']         = FALSE;
+        $this->data['posts']        = NULL;
+        $this->data['user']         = NULL;
+        $this->data['users']        = NULL;
+        $this->data['pages']        = NULL;
+        $this->data['comments']     = NULL;
+
+        if( isset($_COOKIE['CMS_login']) )
+        {
+            $login = $this->input->cookie('CMS_login');
+
+            $this->data['user']         = $this->Model_users->get_by(array('login' => $login))[0];
+            $this->data['user']->role   = $this->Model_users->get_role( ['login' => $login ] );
+            $this->data['auth']         = TRUE;
+        }
+        $this->data['users'] = $this->Model_users->get_users();
+        $this->data['posts'] = $this->Model_posts->get_posts();
     }
 }
--- application/core/MY_Model.php
+++ application/core/MY_Model.php
@@ -17,7 +17,7 @@ class MY_Model extends CI_Model
         parent::__construct();
     }
 
-    public function  get( $id, $single = FALSE )
+    public function get( $id = NULL, $single = FALSE )
     {
         if( $id != NULL )
         {
@@ -29,26 +29,73 @@ class MY_Model extends CI_Model
         } elseif ( $single == TRUE ) {
 
             $method = 'row';
+
         } else {
 
             $method = 'result';
         }
 
-        if( !count( $this->db->ar_orderby ) )
-        {
-            $this->db->order_by( $this->order_by );
-        }
+        /*@sorting line*/
+        //$this->db->order_by( $this->order_by );
 
         return $this->db->get( $this->table_name )->$method();
     }
 
-    public function  get_by( $where, $single = FALSE )
+    public function get_by( $where, $single = FALSE )
     {
         $this->db->where( $where );
 
-        return $this->db->get( NULL, $single );
+        return $this->db->get( $this->table_name, $single )->result();
     }
-    
-    public function  save(){}
-    public function  delete(){}
+
+    public function save($data, $id = NULL)
+    {
+        if($this->timestamps == TRUE)
+        {
+            $now = date('Y-m-d H:i:s');
+            $id || $data['created'] = $now;
+            $data['modified'] = $now;
+        }
+        /**
+         * Insert
+         */
+        if($id == NULL)
+        {
+            !isset($data[$this->primary_key]) || $data[$this->primary_key] = NULL;
+            $this->db->set($data);
+            $this->db->insert($this->table_name);
+            $id = $this->db->insert_id();
+        }
+        /**
+         * Update
+         */
+        else
+        {
+            $filter = $this->primary_filter;
+            $id = $filter($id);
+            $this->db->set($data);
+            $this->db->where($this->primary_key, $id);
+            $this->db->update($this->table_name);
+        }
+
+        return $id;
+    }
+
+    public function delete($id)
+    {
+        $filter = $this->primary_filter;
+        $id = $filter($id);
+
+        if(!$id)
+        {
+            return FALSE;
+        }
+
+        $this->db->where($this->primary_key, $id);
+        $this->db->limit(1);
+        $this->db->delete($this->table_name);
+
+        return TRUE;
+    }
+
 }
