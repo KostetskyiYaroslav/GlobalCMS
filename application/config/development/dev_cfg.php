d97ffaa7d323af00e1ec0f824e31fba0ffdd8b49
Yaroslav <ya.kosteckiy@gmail.com>
2016-05-18 00:32:01 +0300

    CMS personal data(site name, contact email, etc)

--- application/config/development/dev_cfg.php
+++ application/config/development/dev_cfg.php
@@ -1,2 +1,4 @@
 <?php
-$config['site_name'] = 'Web Site';
+$config['site_name']        = 'Web Site';
+$config['contact_email']    = 'ya.kosteckiy@gmail.com';
+$config['domain']           = 'ci3.ukrspace.com';
