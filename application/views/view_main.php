aeef3f1f61b943fc94ed903f92d1644046513247
Yaroslav <ya.kosteckiy@gmail.com>
2016-05-22 01:33:57 +0300

    add sidebar content

--- application/views/view_main.php
+++ application/views/view_main.php
@@ -5,19 +5,19 @@
             <?php foreach ($posts as $post) :?>
                 <div class="single-post">
                     <h2 class="col-xs-9">
-                        <?php echo anchor('posts/view/'.$post->post_slug, $post->post_title); ?>
+                        <?php echo anchor('posts/view/'.$post->post_id, $post->post_title); ?>
                     </h2>
                     <article>
                         <?php if($post->post_attachment == '') : ?>
-                            <img class="col-xs-4 img-thumbnail" src="<?=base_url('assets\uploads\welcome.jpg')?>">
+                            <img class="col-xs-4 img-thumbnail" src="<?=base_url('assets/uploads/static/default-post.png')?>">
                         <?php else : ?>
-                            <img class="col-xs-4 img-thumbnail" src="<?=base_url("assets\\uploads\\".$post->post_attachment)?>">
+                            <img class="col-xs-4 img-thumbnail" src="<?=base_url("assets/uploads/posts/".$post->post_attachment)?>">
                         <?php endif;  ?>
-                        <p class="col-xs-8">
+                        <div class="col-xs-8">
                             <?php $post->post_body = substr( $post->post_body, 0, 200 );?>
                             <?php echo $post->post_body . '...'; ?>
-                            <?php echo anchor('posts/view/'.$post->post_slug, 'Click for more...'); ?>
-                        </p>
+                            <?php echo anchor('posts/view/'.$post->post_id, 'Click for more...'); ?>
+                        </div>
                     </article>
                     <div class="col-xs-9">
                         <p class="popover-title">
