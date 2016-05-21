d3e542421efe794a48d24ec2895f350bb3a5d2ef
Yaroslav <ya.kosteckiy@gmail.com>
2016-05-22 01:34:37 +0300

    add confirmation and restore password

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -44,21 +44,14 @@ class Auth extends MY_Controller
             $user['password']       = do_hash( $user['password'] );
             $user['email']          = $this->input->get_post('email');
             $user['date_created']   = date('Y-m-d H:i:s');
-
-            if( $user['email'] == '')
-            {
-                $user['role_id'] = 5;
-
-            } else {
-
-                $user['role_id'] = 4;
-            }
+            $user['role_id'] = 5;
 
             $existing_user = $this->Model_users->get_by(array('login' => $user['login'] ));
 
             if( $existing_user == NULL && $this->form_validation->run() == TRUE )
             {
                 $this->Model_users->save($user);
+                $this->_send_confirm_latter($user['email'], $user['login']);
                 $this->load->view('auth/view_success_reg');
 
             } elseif ( $this->form_validation->run() == FALSE ) {
@@ -153,6 +146,143 @@ class Auth extends MY_Controller
             redirect('main');
         }
 
+    }
+
+    public function confirm( $key, $login )
+    {
+        $this->load->helper('form');
+        if($key != NULL && $login != NULL )
+        {
+            $this->load->model('Model_confirmation');
+            $confirmation = $this->Model_confirmation->get_by(['key' => $key, 'login' => $login], TRUE);
+            if(empty($confirmation ))
+            {
+                redirect('/');
+
+            } else {
+
+                $confirmation = $confirmation[0];
+                $user = $this->Model_users->get_by(['login'=>$confirmation->login], true);
+                if(empty($user))
+                {
+                    redirect('/');
+                } else
+                    $user = $user[0];
+
+                $this->Model_users->save(['role_id'=>4], $user->id);
+                $this->Model_confirmation->delete($confirmation->id);
+                $this->data['message'] = "You have successfully activate you'r account!";
+                $this->load->view( 'components/view_header',
+                    [
+                        'title' => 'Okay :)',
+                        'auth'  => $this->data['auth'],
+                        'user'  => $this->data['user']
+                    ]
+                );
+                $this->load->view('auth/view_confirmation_success',
+                    [
+                        'message' =>  $this->data['message']
+                    ]
+                );
+            }
+        } else
+        {
+            redirect('/');
+        }
+    }
+
+    public function restore( $login = NULL)
+    {
+        $this->load->helper('form');
+
+        if( $login != NULL)
+        {
+            $restore_user = $this->Model_users->get_by([ 'login' => $login ], TRUE);
+
+            if( !empty($restore_user) )
+            {
+                $restore_user = $restore_user[0];
+            }
+            $this->_send_restore_latter( $restore_user );
+
+        } else {
+
+            echo form_open('/');
+            echo form_input(['type'=>'text', 'name'=>'restore-login', 'id'=>'restore-login']);
+            echo form_input(['type'=>'email', 'name'=>'restore-email', 'id'=>'restore-email']);
+            echo form_input(['type'=>'submit']);
+            echo form_close();
+        }
+    }
+
+    private function _send_confirm_latter($to, $login)
+    {
+        $this->load->model(['Model_confirmation', 'Model_templates']);
+        $this->load->library('email');
+
+        $key    = do_hash($login);
+        $url    = base_url("auth/confirm/$key/$login");
+        $template = $this->Model_templates->get_by(['name' => 'Confirmation'], TRUE);
+
+        if( !empty($template))
+        {
+            $template = $template[0];
+            $template->template = str_replace('{login}', $login, $template->template );
+            $template->template = str_replace('{here}', "<a href='$url'>here</a>", $template->template );
+
+        } else {
+
+            $template = $url;
+        }
+
+        $data = [
+            'key'   => $key,
+            'login' => $login
+        ];
+        $this->Model_confirmation->save($data);
+
+        $this->email->mailtype = 'html';
+        $this->email->from(config_item('contact_email'), 'Account Manager');
+        $this->email->to($to);
+        $this->email->subject('Confirmation');
+        $this->email->message($template->template);
+
+        $this->email->send();
+
+    }
+
+    private function _send_restore_latter($restore_user)
+    {
+        $this->load->model('Model_templates');
+        $this->load->helper(['security']);
+        $this->load->library('email');
+
+        $new_pass   = rand(666666,999999);
+        $hash_pass  = do_hash($new_pass);
+        $template = $this->Model_templates->get_by(['name'=>'Restore'], TRUE);
+        if(!empty($template))
+        {
+            $template = $template[0];
+            $template->template = str_replace('{login}', $restore_user->login, $template->template);
+            $template->template = str_replace('{password}', $new_pass, $template->template);
+        }
+        #region Change Password
+
+        $data = [
+            'password' => $hash_pass,
+        ];
+        $this->Model_users->save($data, $restore_user->id);
+
+        #endregion
+
+        $this->email->from(config_item('contact_email'), 'Account Manager');
+        $this->email->to($restore_user->email);
+        $this->email->mailtype = 'html';
+
+        $this->email->subject('Restore password');
+        $this->email->message($template->template);
+
+        $this->email->send();
     }
 
 }
