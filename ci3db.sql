baa520278665e370fca407f7ce998b0e5854a25b
Yaroslav <ya.kosteckiy@gmail.com>
2016-05-22 01:49:01 +0300

    MySQL dump

--- ci3db.sql
+++ ci3db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: localhost
--- Час створення: Трв 19 2016 р., 01:08
+-- Час створення: Трв 22 2016 р., 01:46
 -- Версія сервера: 5.5.49-0+deb8u1
 -- Версія PHP: 5.6.20-0+deb8u1
 
@@ -43,6 +43,39 @@ INSERT INTO `categories` (`id`, `name`, `tags`, `role_id`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Структура таблиці `comments`
+--
+
+CREATE TABLE IF NOT EXISTS `comments` (
+`id` int(11) NOT NULL,
+  `body` varchar(255) NOT NULL,
+  `date` datetime NOT NULL,
+  `author_id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблиці `confirmation`
+--
+
+CREATE TABLE IF NOT EXISTS `confirmation` (
+`id` int(11) NOT NULL,
+  `key` varchar(255) NOT NULL,
+  `login` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+
+--
+-- Дамп даних таблиці `confirmation`
+--
+
+INSERT INTO `confirmation` (`id`, `key`, `login`) VALUES
+(7, '28bffb541785ee1b676df6981702bed9830d913d', 'Vovat');
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблиці `posts`
 --
 
@@ -56,20 +89,15 @@ CREATE TABLE IF NOT EXISTS `posts` (
   `tags` varchar(255) DEFAULT NULL,
   `category_id` int(11) DEFAULT '1',
   `slug` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
 
 --
 -- Дамп даних таблиці `posts`
 --
 
 INSERT INTO `posts` (`id`, `title`, `body`, `author_id`, `attachment`, `date`, `tags`, `category_id`, `slug`) VALUES
-(1, 'First Post', 'asdfsdfhgf', 8, '', '2016-05-15 12:11:14', '', 1, 'first-post'),
-(2, 'Welcome!', 'We very happy to see you here! Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum', 7, '', '2016-06-15 11:11:14', '', 1, 'welcome'),
-(3, 'Welco231me!', 'We very happy to see you here! Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum', 7, '', '2016-05-15 13:11:14', '', 1, 'welcome1'),
-(4, 'tet', 'We very happy to see you here! Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum', 9, '', '2016-05-15 14:11:14', '', 1, 'welcome1'),
-(8, 'Welcome!2', '56456456456', 7, '', '2016-05-15 15:11:14', '', 1, 'welcome'),
-(9, '1123', 'We very happy to see you here! Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum', 9, '', '2016-05-15 20:11:14', '', 1, 'welcome1'),
-(10, 'Welco231me!', 'We very happy to see you here! Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum', 7, '', '2016-05-15 16:11:14', '', 1, 'welcome1');
+(18, 'Hello World!', '<p><span style="text-decoration: line-through;">asdjkfguklajdhgjklsahdfjk asdfjkl dh;sadjkf bhakdagnjdfgkLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</span></p>', 9, 'default-post.png', '2016-05-19 15:19:19', '', 1, 'HelloWorld'),
+(19, 'Welcome', '<p style="text-align: right;"><em>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has sur<strong>vived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</strong> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</em></p>', 11, '', '2016-05-19 15:19:19', ' ', 1, 'welcome');
 
 -- --------------------------------------------------------
 
@@ -99,6 +127,26 @@ INSERT INTO `roles` (`id`, `name`, `access_lvl`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Структура таблиці `templates`
+--
+
+CREATE TABLE IF NOT EXISTS `templates` (
+`id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `template` text NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Дамп даних таблиці `templates`
+--
+
+INSERT INTO `templates` (`id`, `name`, `template`) VALUES
+(1, 'Confirmation', 'Dear, {login}!<br>We are very grab for you in our community!<br>To confirm you account click {here}!'),
+(2, 'Restore', 'Dear, {login}!<br>If you don''t restore your password tall about it Administrator!forgot for this massage!<br>This is your new password: {password}\r\n');
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблиці `users`
 --
 
@@ -109,18 +157,18 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email` varchar(100) NOT NULL,
   `role_id` int(11) DEFAULT '5',
   `date_created` datetime NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=68 DEFAULT CHARSET=utf8;
 
 --
 -- Дамп даних таблиці `users`
 --
 
 INSERT INTO `users` (`id`, `login`, `password`, `email`, `role_id`, `date_created`) VALUES
-(7, 'Adminman', 'b692b4fc2b4bb8f20b1f2ef0318f1ed599e05453', 'Adminman@admin.com', 1, '2015-05-18 11:48:33'),
-(9, 'Kosteckiy', '51d291167e983ab74e5cfbb7cec486003ba11725', 'ya.kosteckiy@gmail.com', 1, '2013-05-18 11:48:33'),
-(10, 'kostecki', '042dc4512fa3d391c5170cf3aa61e6a638f84342', 'yaroslavxxxx@yandex.ru', 2, '2012-05-18 11:48:33'),
-(11, 'Alina', '356a192b7913b04c54574d18c28d46e6395428ab', 'Alina@bk.ru', 4, '2011-05-18 11:48:33'),
-(12, 'Maxim', '356a192b7913b04c54574d18c28d46e6395428ab', 'Max@mail.bk', 3, '2016-05-18 11:48:33');
+(9, 'Kosteckiy', '28ac44dc7400b63ccf1d3508b4e28c6fa228bddb', 'ya.kosteckiy@gmail.com', 1, '2013-05-18 11:48:33'),
+(60, '11111', '356a192b7913b04c54574d18c28d46e6395428ab', 'ya.kosteckiy@gmail.com', 5, '2016-05-22 00:54:51'),
+(61, '111111', '356a192b7913b04c54574d18c28d46e6395428ab', 'ya.kosteckiy@gmail.com', 4, '2016-05-22 00:55:03'),
+(62, '22222', '356a192b7913b04c54574d18c28d46e6395428ab', 'ya.kosteckiy@gmail.com', 4, '2016-05-22 00:57:22'),
+(63, '00000', '356a192b7913b04c54574d18c28d46e6395428ab', 'ya.kosteckiy@gmail.com', 4, '2016-05-22 00:58:33');
 
 --
 -- Індекси збережених таблиць
@@ -133,6 +181,18 @@ ALTER TABLE `categories`
  ADD PRIMARY KEY (`id`);
 
 --
+-- Індекси таблиці `comments`
+--
+ALTER TABLE `comments`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Індекси таблиці `confirmation`
+--
+ALTER TABLE `confirmation`
+ ADD PRIMARY KEY (`id`);
+
+--
 -- Індекси таблиці `posts`
 --
 ALTER TABLE `posts`
@@ -142,6 +202,12 @@ ALTER TABLE `posts`
 -- Індекси таблиці `roles`
 --
 ALTER TABLE `roles`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Індекси таблиці `templates`
+--
+ALTER TABLE `templates`
  ADD PRIMARY KEY (`id`);
 
 --
@@ -160,20 +226,35 @@ ALTER TABLE `users`
 ALTER TABLE `categories`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
+-- AUTO_INCREMENT для таблиці `comments`
+--
+ALTER TABLE `comments`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблиці `confirmation`
+--
+ALTER TABLE `confirmation`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
+--
 -- AUTO_INCREMENT для таблиці `posts`
 --
 ALTER TABLE `posts`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=20;
 --
 -- AUTO_INCREMENT для таблиці `roles`
 --
 ALTER TABLE `roles`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
 --
+-- AUTO_INCREMENT для таблиці `templates`
+--
+ALTER TABLE `templates`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
 -- AUTO_INCREMENT для таблиці `users`
 --
 ALTER TABLE `users`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=51;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=68;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
