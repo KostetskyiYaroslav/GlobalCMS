1ddd837faec036f596e35b5216671e2eef967ad0
Yaroslav <ya.kosteckiy@gmail.com>
2016-05-26 01:07:06 +0300

    templates,comments,categories were added to controlles

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -43,6 +43,10 @@ class Admin extends MY_Controller
                 $this->load->view('menu/view_' . $menu, ['comments' => $this->data['comments'], 'current_user' => $this->data['user']]);
                 break;
 
+            case 'categories':
+                $this->load->view('menu/view_' . $menu, ['categories' => $this->data['categories'], 'current_user' => $this->data['user']]);
+                break;
+
             case 'templates':
                 $this->load->view('menu/view_' . $menu, ['templates' => $this->data['templates'], 'current_user' => $this->data['user']]);
                 break;
@@ -103,6 +107,7 @@ class Admin extends MY_Controller
         );
 
         if (isset($_POST['new-login'])) {
+
             if (isset($_POST['new-login']) && $_POST['new-login'] != '') {
                 $login = $this->input->get_post('new-login');
 
@@ -149,7 +154,14 @@ class Admin extends MY_Controller
 
                 if ($existing_user == NULL && $this->form_validation->run() == TRUE) {
                     $this->Model_users->save($data);
+
                 }
+
+                $this->load->view('user/view_new',
+                    [
+                        'message' => $message
+                    ]
+                );
             } else {
                 $this->load->view('user/view_new',
                     [
@@ -223,14 +235,165 @@ class Admin extends MY_Controller
 
     #endregion
 
-    #region Templates Munipulation
+    #region Templates Manipulation
 
-    public function templates($id = NULL)
+    public function templates_delete($id)
     {
-        if($id == NULL)
-        {
+        $this->Model_templates->delete($id);
+        redirect('admin/dashboard/templates');
+    }
 
+    public function templates_single($id)
+    {
+        $single_template = $this->Model_templates->get($id,true);
+
+        $this->load->view('components/view_header',
+            [
+                'title' => 'Single Templates',
+                'auth' => $this->data['auth'],
+                'user' => $this->data['user']
+            ]
+        );
+
+        $this->load->view('templates/view_single',
+            array
+            (
+                'current_user' => $this->data['user'],
+                'single_template' => $single_template,
+                'current_user' => $this->data['user']
+            )
+        );
+    }
+
+    public function templates_save()
+    {
+        $data = array();
+        $condition = true;
+        $name = $template = null;
+        $message = '';
+
+        $this->load->view('components/view_header',
+            [
+                'title' => 'New User',
+                'auth' => $this->data['auth'],
+                'user' => $this->data['user']
+            ]
+        );
+
+        if (isset($_POST['new-login'])) {
+
+            if (isset($_POST['new-login']) && $_POST['new-login'] != '') {
+                $login = $this->input->get_post('new-login');
+
+            } else {
+
+                $condition = false;
+            }
+            if (isset($_POST['new-password']) && $_POST['new-password'] != '') {
+                $password = $this->input->get_post('new-password');
+
+            } else {
+
+                $condition = false;
+            }
+            if (isset($_POST['new-email']) && $_POST['new-email'] != '') {
+                $email = $this->input->get_post('new-email');
+
+            }
+            if (isset($_POST['new-role-id']) && $_POST['new-role-id'] != '') {
+                $role_id = $this->input->get_post('new-role-id');
+
+            }
+            if (isset($_POST['new-date-created']) && $_POST['new-date-created'] != '') {
+                $date_created = $this->input->get_post('new-date-created');
+
+            } else {
+
+                $date_created = date('Y-m-d H:i:s');
+            }
+
+            $data = array
+            (
+                'login' => $login,
+                'password' => $password,
+                'email' => $email,
+                'role_id' => $role_id,
+                'date_created' => $date_created
+            );
+
+            if ($condition) {
+                $this->Model_users->save($data);
+                $message = 'New User ' . $login . ' successfully created!';
+                $existing_user = $this->Model_users->get_by(array('login' => $data['login']));
+
+                if ($existing_user == NULL && $this->form_validation->run() == TRUE) {
+                    $this->Model_users->save($data);
+
+                }
+
+                $this->load->view('user/view_new',
+                    [
+                        'message' => $message
+                    ]
+                );
+            } else {
+                $this->load->view('user/view_new',
+                    [
+                        'message' => 'Some error!'
+                    ]
+                );
+            }
+        } else {
+            $this->load->view('user/view_new',
+                [
+                    'message' => ''
+                ]
+            );
         }
+
+//        Update $pages = $this->Model_posts->save($data, 3);
+    }
+
+    public function templates_edit()
+    {
+        $data = array();
+        $condition = TRUE;
+        $id = $this->uri->segment(3);
+        $password = $email = $role_id = $date_created = $message = '';
+        $update_template = $this->Model_templates->get($id, TRUE);
+
+        $this->load->view('components/view_header',
+            [
+                'title' => 'Edit User',
+                'auth' => $this->data['auth'],
+                'user' => $this->data['user']
+            ]
+        );
+
+        if (isset($_POST['update-template'])) {
+            $template = $id = NULL;
+            $id = $this->input->get_post('update-id');
+            $template = $this->input->get_post('update-template');
+
+            $data =
+                [
+                    'template' => $template
+                ];
+
+            $this->Model_templates->save($data, $id);
+            $message = 'Template successfully updated!';
+        }
+
+        if (empty($update_template)) {
+            $message = 'Template dose not exist!';
+        }
+
+        $this->load->view('templates/view_edit',
+            [
+                'update_template' => $update_template,
+                'message' => $message
+            ]
+        );
 
     }
 
@@ -246,7 +409,7 @@ class Admin extends MY_Controller
     public function post_single($id)
     {
         $single_post    = $this->Model_posts->get_posts($id, TRUE);
-
+        $single_post->comments = $this->Model_comments->get_post_comments($single_post->post_id);
         $this->load->view( 'components/view_header',
             [
                 'title' => 'Single Post',
@@ -254,6 +417,7 @@ class Admin extends MY_Controller
                 'user'  => $this->data['user']
             ]
         );
+
         $this->load->view('posts/view_single',
             array
             (
@@ -489,6 +653,176 @@ class Admin extends MY_Controller
 
     #endregion
 
+    #region Category Manipulation
+
+    public function categories_delete($id)
+    {
+        $this->Model_categories->delete($id);
+        redirect('admin/dashboard/categories');
+    }
+
+    public function categories_single($id)
+    {
+        $category_posts = $this->Model_posts->get_post_by(['category_id'=>$id]);
+
+        if (isset($_POST['edit'])) {
+            $edit_field = '';
+        }
+
+        $this->load->view('components/view_header',
+            [
+                'title' => 'Single Category',
+                'auth' => $this->data['auth'],
+                'user' => $this->data['user']
+            ]
+        );
+
+        $this->load->view('categories/view_single',
+            array
+            (
+                'current_user' => $this->data['user'],
+                'categories' => $this->data['categories'],
+                'category_posts' => $category_posts
+            )
+        );
+
+    }
+
+    public function categories_save()
+    {
+        $data = array();
+        $condition = true;
+        $login = $password = $email = $role_id = $date_created = null;
+        $message = '';
+
+        $this->load->view('components/view_header',
+            [
+                'title' => 'New User',
+                'auth' => $this->data['auth'],
+                'user' => $this->data['user']
+            ]
+        );
+
+        if (isset($_POST['new-login'])) {
+            if (isset($_POST['new-login']) && $_POST['new-login'] != '') {
+                $login = $this->input->get_post('new-login');
+
+            } else {
+
+                $condition = false;
+            }
+            if (isset($_POST['new-password']) && $_POST['new-password'] != '') {
+                $password = $this->input->get_post('new-password');
+
+            } else {
+
+                $condition = false;
+            }
+            if (isset($_POST['new-email']) && $_POST['new-email'] != '') {
+                $email = $this->input->get_post('new-email');
+
+            }
+            if (isset($_POST['new-role-id']) && $_POST['new-role-id'] != '') {
+                $role_id = $this->input->get_post('new-role-id');
+
+            }
+            if (isset($_POST['new-date-created']) && $_POST['new-date-created'] != '') {
+                $date_created = $this->input->get_post('new-date-created');
+
+            } else {
+
+                $date_created = date('Y-m-d H:i:s');
+            }
+
+            $data = array
+            (
+                'login' => $login,
+                'password' => $password,
+                'email' => $email,
+                'role_id' => $role_id,
+                'date_created' => $date_created
+            );
+
+            if ($condition) {
+                $this->Model_users->save($data);
+                $message = 'New User ' . $login . ' successfully created!';
+                $existing_user = $this->Model_users->get_by(array('login' => $data['login']));
+
+                if ($existing_user == NULL && $this->form_validation->run() == TRUE) {
+                    $this->Model_users->save($data);
+                }
+            } else {
+                $this->load->view('user/view_new',
+                    [
+                        'message' => 'Some error!'
+                    ]
+                );
+            }
+        } else {
+            $this->load->view('user/view_new',
+                [
+                    'message' => ''
+                ]
+            );
+        }
+
+//        Update $pages = $this->Model_posts->save($data, 3);
+    }
+
+    public function categories_edit()
+    {
+        $data = array();
+        $condition = TRUE;
+        $login = $this->uri->segment(3);
+        $password = $email = $role_id = $date_created = $message = '';
+        $update_user = $this->Model_users->get_by(['login' => $login], TRUE);
+
+        $this->load->view('components/view_header',
+            [
+                'title' => 'Edit User',
+                'auth' => $this->data['auth'],
+                'user' => $this->data['user']
+            ]
+        );
+
+        if (isset($_POST['update-email'])) {
+            $email = $password = $role_id = $date_created = $id = NULL;
+            $id = $this->input->get_post('update-id');
+            $email = $this->input->get_post('update-email');
+            $password = $this->input->get_post('update-password');
+            $role_id = $this->input->get_post('update-role-id');
+            $date_created = $this->input->get_post('update-date');
+
+            $data =
+                [
+                    'email' => $email,
+                    'password' => $password,
+                    'role_id' => $role_id,
+                    'date_created' => $date_created
+                ];
+
+            $this->Model_users->save($data, $id);
+            $message = 'User successfully updated!';
+        }
+
+        if (!empty($update_user)) {
+            $update_user = $update_user[0];
+
+        } else {
+
+            $message = 'User dose not exist!';
+        }
+
+        $this->load->view('user/view_edit',
+            [
+                'update_user' => $update_user,
+                'message' => $message
+            ]
+        );
+
+    }
+
+    #endregion
 
     private function _make_slug($title)
     {
