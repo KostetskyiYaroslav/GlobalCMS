84f7c8d88a7f27d74b3733fa9b26a547ab5f0a85
Yaroslav <ya.kosteckiy@gmail.com>
2016-05-26 01:06:10 +0300

    added new filed

--- application/config/development/dev_cfg.php
+++ application/config/development/dev_cfg.php
@@ -2,3 +2,4 @@
 $config['site_name']        = 'Web Site';
 $config['contact_email']    = 'ya.kosteckiy@gmail.com';
 $config['domain']           = 'ci3.ukrspace.com';
+$config['keywords']         = 'SofgGroup, sg, IT Company, PHP Development, PHP Academy';
