e97ba42e07da72f9cf6711217508da31ad1c3815
Yaroslav <ya.kosteckiy@gmail.com>
2016-06-06 00:41:03 +0300

    added install operation, updated removing process(before remove DB now it remove fro DB and all files in folder)

--- application/controllers/Widgets.php
+++ application/controllers/Widgets.php
@@ -33,4 +33,64 @@ class Widgets extends MY_Controller
         );
     }
 
+    function install()
+    {
+        $config['upload_path']          = './application/views/settings/widgets/';
+        $config['allowed_types']        = 'zip';
+        $config['max_size']             = 5000;
+        $config['overwrite']            = TRUE;
+        $config['file_ext_tolower']     = TRUE;
+
+        $this->load->library('upload', $config);
+
+        if ( ! $this->upload->do_upload('userfile'))
+        {
+            $error = array('error' => $this->upload->display_errors());
+            $this->load->view('components/view_header',
+                [
+                    'title' => 'Single Settings',
+                    'auth' => $this->data['auth'],
+                    'user' => $this->data['user'],
+                    'widgets' => $this->data['widgets']
+                ]
+            );
+            $this->load->view('settings/widgets/view_upload',
+                array
+                (
+                    'categories' => $this->data['categories'],
+                    'widgets' => $this->data['widgets'],
+                    'error' => $error
+                )
+            );
+        }
+        else
+        {
+            $upload_path = './application/views/settings/widgets/';
+            $data = array('upload_data' => $this->upload->data());
+            $raw_name = $data['upload_data']['raw_name'];
+            mkdir($upload_path.$raw_name, 0700);
+
+            $zip = new ZipArchive;
+            $file = $data['upload_data']['full_path'];
+            chmod($file, 0777);
+            if ($zip->open($file) === TRUE) {
+                $zip->extractTo($upload_path.$raw_name);
+                $zip->close();
+            }
+
+            rename( $upload_path.$raw_name.'/'.$raw_name.'.php',
+                    $upload_path.$raw_name.'/widget.php' );
+            unlink($upload_path.$this->upload->file_name);
+
+            $this->Model_widgets->save_get_widgets_abs(
+                [
+                    'name' => ucfirst($raw_name),
+                    'path' => $raw_name,
+                    'role_id' => '6'
+                ]
+            );
+            redirect('/settings/widgets');
+        }
+
+    }
 }
