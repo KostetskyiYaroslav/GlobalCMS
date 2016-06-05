ee37e8aac23f9ca84d22766bf6cd8b4a1f67089d
Yaroslav <ya.kosteckiy@gmail.com>
2016-06-06 00:37:12 +0300

    added function to save new widgets

--- application/models/Model_widgets.php
+++ application/models/Model_widgets.php
@@ -47,4 +47,33 @@ class Model_widgets extends MY_Model
 
         return $this->db->get( 'widgets_abs', $single )->result();
     }
+
+    public function save_get_widgets_abs($data, $id = NULL)
+    {
+        if($this->timestamps == TRUE)
+        {
+            $now = date('Y-m-d H:i:s');
+            $id || $data['created'] = $now;
+            $data['modified'] = $now;
+        }
+
+        if($id == NULL)
+        {
+            !isset($data[$this->primary_key]) || $data[$this->primary_key] = NULL;
+            $this->db->set($data);
+            $this->db->insert('widgets_abs');
+            $id = $this->db->insert_id();
+        }
+
+        else
+        {
+            $filter = $this->primary_filter;
+            $id = $filter($id);
+            $this->db->set($data);
+            $this->db->where($this->primary_key, $id);
+            $this->db->update('widgets_abs');
+        }
+
+        return $id;
+    }
 }
