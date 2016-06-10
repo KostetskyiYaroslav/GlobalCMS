f3e5095b48a097f99edc9e8d52d46c012db223c7
Yaroslav Kostetskiy <ya.kosteckiy@gmail.com>
2016-06-10 22:47:45 +0200

    added navigation model

---
+++ application/models/Model_navigation.php
@@ -0,0 +1,12 @@
+<?php
+
+class Model_navigation extends MY_Model
+{
+    protected $table_name = 'navigation';
+    protected $primary_key = 'id';
+    protected $primary_filter = 'intval';
+    protected $order_by = '';
+    protected $rules = array();
+    protected $timestamps = FALSE;
+
+}
